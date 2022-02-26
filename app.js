ddec56cfa71b0e42a2cce9e7a2bd79b5452138bb
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-02-26 12:15:36 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const { Composer } = require('micro-bot')
+
+const bot = new Composer()
+
+bot.on('new_chat_members',ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id , `Hello ${ctx.chat.id}, \nWelcome to Ready4player Multi-Metaverse Gaming `).catch((e)=>console.log(e))
+})
+
+bot.command('website',ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id , `https://ready4player.io`).catch((e)=>console.log(e))
+})
+
+bot.command('whitepaper',ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id , `https://ready4player.io/wp-content/uploads/2022/02/Ready4Player-Official-Whitepaper.pdf`).catch((e)=>console.log(e))
+})
+
+bot.command('report',ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id , `Message does not set yet`).catch((e)=>console.log(e))
+})
+
+bot.command('chart',ctx=>{
+    ctx.telegram.sendMessage(ctx.chat.id , `Message does not set yet`).catch((e)=>console.log(e))
+})
+
+
+module.exports = bot
